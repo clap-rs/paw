2562bfc6522bf8923163821b93878e9d17896101
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-04-22 00:34:49 +0200

    add clap support
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- paw-clap/src/lib.rs
+++ paw-clap/src/lib.rs
@@ -2,3 +2,30 @@
 
 #![recursion_limit = "512"]
 #![warn(missing_debug_implementations, nonstandard_style, rust_2018_idioms)]
+
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::{quote, quote_spanned};
+use syn::spanned::Spanned;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Paw)]
+pub fn paw(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident;
+
+    let result = quote! {
+        impl paw::TryParse for #name {
+            /// Error type.
+            type Error = std::io::Error;
+
+            /// Try to parse an input to a type.
+            fn try_parse() -> Result<Self, Self::Error> {
+                Ok(<#name as structopt::StructOpt>::from_args())
+            }
+        }
+    };
+
+    result.into()
+}
