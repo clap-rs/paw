e96d8bec0548e1b5274dc866d4519fab1f26163e
Dylan DPC <dylan.dpc@gmail.com>
2019-04-23 21:37:02 +0200

    Args is now an exported type of the crate

--- examples/new_type.rs
+++ examples/new_type.rs
@@ -1,39 +1,15 @@
-use std::env::Args;
 use std::error::Error;
 use std::io::prelude::*;
 use std::net::TcpListener;
 
-//! To run this example (e.g. with port 8080): 
-//! ```
-//! cargo run --example new_type -- --port 8080 
-//! ```
-//! (note that the port should be separated by a space, and port=8080 doesn't work with this code)
-
-#[derive(Debug)]
-pub struct ArgWrapper(pub Args);
-
-impl paw::TryParse for ArgWrapper {
-    type Error = std::io::Error;
-    fn try_parse() -> Result<Self, Self::Error> {
-        Ok(Self(std::env::args()))
-    }
-}
-
-#[derive(Debug)]
-pub struct ArgNotFoundError {
-    arg: String,
-}
-
-impl std::fmt::Display for ArgNotFoundError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "the argument {} is not passed", self.arg)
-    }
-}
-
-impl Error for ArgNotFoundError {}
+/// To run this example (e.g. with port 8080):
+/// ```
+/// cargo run --example new_type -- --port 8080
+/// ```
+/// (note that the port should be separated by a space, and port=8080 doesn't work with this code)
 
 #[paw::main]
-fn main(args: ArgWrapper) -> Result<(), Box<dyn Error>> {
+fn main(args: paw::Args) -> Result<(), Box<dyn Error>> {
     // Rust reads the arguments to the process as an env::Args which is
     // an iterator over stirngs. It contains the arguments as strings if
     // they are separated by space. Ensure you give the argument as --port 8080
--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,27 @@
 #[doc(inline)]
 #[cfg(not(test))] // NOTE: exporting main breaks tests, we should file an issue.
 pub use paw_attributes::main;
+use std::env::Args as EnvArgs;
+use std::iter::Iterator;
 
+#[derive(Debug)]
+/// Args is a wrapper over env::Args which is an iterator over the arguments to a process.
+pub struct Args(pub EnvArgs);
+
+impl Iterator for Args {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+}
+
+impl TryParse for Args {
+    type Error = std::io::Error;
+    fn try_parse() -> Result<Self, Self::Error> {
+        Ok(Self(std::env::args()))
+    }
+}
 /// Try parsing.
 pub trait TryParse: Sized {
     /// Error type.
@@ -21,3 +41,17 @@ pub trait TryParse: Sized {
     /// Try to parse an input to a type.
     fn try_parse() -> Result<Self, Self::Error>;
 }
+
+#[derive(Debug)]
+/// An error which is returned when an argument isn't passed to the process
+pub struct ArgNotFoundError {
+    arg: String,
+}
+
+impl std::fmt::Display for ArgNotFoundError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "the argument {} is not passed", self.arg)
+    }
+}
+
+impl std::error::Error for ArgNotFoundError {}
