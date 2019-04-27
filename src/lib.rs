cc9aa4ecb516e05e50d4566fe8a29093e21728d5
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-04-27 14:14:51 +0200

    ArgsOs
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,9 @@
 //! ```rust
 //! ```
 
-use std::env::Args as EnvArgs;
+use std::env::{Args as StdArgs, ArgsOs as StdArgsOs};
 use std::iter::Iterator;
+use std::ffi::OsString;
 use std::fmt;
 
 #[doc(inline)]
@@ -28,7 +29,7 @@ pub use paw_raw::ParseArgs;
 /// [`String`]: ../string/struct.String.html
 /// [`std::env::Args`]: https://doc.rust-lang.org/std/env/struct.Args.html
 pub struct Args {
-    inner: EnvArgs,
+    inner: StdArgs,
 }
 
 impl fmt::Debug for Args {
@@ -59,5 +60,47 @@ impl ParseArgs for Args {
     type Error = std::io::Error;
     fn try_parse() -> Result<Self, Self::Error> {
         Ok(Self { inner: std::env::args() })
+    }
+}
+
+/// An iterator over the arguments of a process, yielding an [`OsString`] value for each argument.
+///
+/// This is a wrapper over [`std::env::ArgsOs`] which is an iterator over the arguments to a
+/// process. See its documentation for more.
+///
+/// [`OsString`]: ../ffi/struct.OsString.html
+/// [`std::env::ArgsOs`]: https://doc.rust-lang.org/std/env/struct.ArgsOs.html
+pub struct ArgsOs {
+    inner: StdArgsOs,
+}
+
+impl fmt::Debug for ArgsOs {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.inner.fmt(f)
+    }
+}
+
+impl Iterator for ArgsOs {
+    type Item = OsString;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next()
+    }
+}
+
+impl ExactSizeIterator for ArgsOs {
+    fn len(&self) -> usize { self.inner.len() }
+}
+
+impl DoubleEndedIterator for ArgsOs {
+    fn next_back(&mut self) -> Option<OsString> {
+        self.inner.next_back()
+    }
+}
+
+impl ParseArgs for ArgsOs {
+    type Error = std::io::Error;
+    fn try_parse() -> Result<Self, Self::Error> {
+        Ok(Self { inner: std::env::args_os() })
     }
 }
