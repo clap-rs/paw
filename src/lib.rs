38ed84ee5fc484254153e7f5224da91f8e21fd89
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-05-06 10:54:29 +0200

    clean up main example
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -16,41 +16,10 @@
 //! ## Examples
 //! __Create a parser from scratch__
 //! ```rust
-//! use std::io::{self, prelude::*};
-//! use std::net::TcpListener;
-//!
-//! struct Args {
-//!     port: u16,
-//!     address: String,
-//! }
-//!
 //! #[paw::main]
-//! fn main(args: Args) -> Result<(), Box<dyn std::error::Error>> {
-//!     let listener = TcpListener::bind((args.address.as_str(), args.port))?;
-//!     println!("listening on {}", listener.local_addr()?);
-//!
-//!     for stream in listener.incoming() {
-//!         stream?.write(b"hello world!")?;
-//!     }
-//!     Ok(())
-//! }
-//!
-//! impl paw::ParseArgs for Args {
-//!     type Error = Box<dyn std::error::Error>;
-//!
-//!     fn parse_args() -> Result<Self, Self::Error> {
-//!         let mut args = std::env::args().skip(1);
-//!
-//!         let address = args
-//!             .next()
-//!             .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "the address arg is missing"))?;
-//!
-//!         let port = args
-//!             .next()
-//!             .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "the port arg is missing"))?
-//!             .parse()?;
-//!
-//!         Ok(Self { address, port })
+//! fn main(args: paw::Args) {
+//!     for arg in args {
+//!         println!("{:?}", arg);
 //!     }
 //! }
 //! ```
